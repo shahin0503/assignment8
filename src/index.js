5df3386e5fc6903b3d268d8c8d3a0b01221f9efb
unknown <shahinbanu.bodeliwala.at@gmail.com>
2023-10-28 12:07:42 +0530

    create blog

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 
 const userRouter = require('./routers/user')
+const blogRouter = require('./routers/blog')
 
 const app = express()
 app.use(express.json())
 app.use(userRouter)
+app.use(blogRouter)
 
 const port = process.env.PORT || 8080
 
---
+++ src/routers/blog.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const admin = require('../firebase')
+const router = new express.Router()
+
+const db = admin.firestore()
+
+
+router.post('/create', async (req, res) => {
+    try {
+        const userJson = {
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author,
+            timestamp: admin.firestore.FieldValue.serverTimestamp()
+        };
+        const response = await db.collection("blogs").add(userJson);
+        res.send(response);
+    } catch (error) {
+        res.status(500).send("An error occurred, please try again later");
+    }
+})
+
+
+module.exports = router
